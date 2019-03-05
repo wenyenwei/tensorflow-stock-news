6978814645d414b8e72750f6c86b0a7b0bbf6fd8
Wenyen Wei <weiwenyen1123@gmail.com>
2019-03-05 21:59:41 +1100

    :bug: preprocessing and update batch

--- rnews2.py
+++ rnews2.py
@@ -4,25 +4,31 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
 
+
+# TODO: 
+# 1. rewrite pre-processing
+# 2. fix batch problem
+
 class MainRNN():
 	def __init__(self):
 		self.data_time_range=7
-		self.batch_size=self.data_time_range
+		self.seq_size=self.data_time_range
 		self.hidden_layer=1
 		self.output_feature_size=1
 		self.epochs=3
-		self.data_size=self.batch_size*1000
+		self.data_point=88434
+		self.data_size=self.seq_size*self.data_point
 		self.error_rate=0.1
+		self.batch_size=2
 
 	def x_y_to_seq(self, X, Y):
 		# X = [[[yesterday_stock_data(5)], [today_stock_data(5)], [tomorrow_stock_data(5)], ...batch_size], [repeat]]
 		newX = []
 		newY = []
-		print('lenX: ' + str(len(X) / self.batch_size))
-		for i in range(int(len(X) / self.batch_size)):
-			print((i + 1) * self.batch_size - 1)
-			newX.append(X[i * self.batch_size : (i + 1) * self.batch_size])
-			newY.append(Y[(i + 1) * self.batch_size - 1])
+		for i in range(int(len(X) / self.seq_size)):
+			print((i + 1) * self.seq_size - 1)
+			newX.append(X[i * self.seq_size : (i + 1) * self.seq_size])
+			newY.append(Y[(i + 1) * self.seq_size - 1])
 
 		return newX, newY
 
@@ -42,19 +48,25 @@ class MainRNN():
 		    y_val = df.loc[(df['Name'] == "AAPL") & (df['date'] == row['date'])].values[0][4] # close price
 		    Y.append(y_val)
 		    print(index)
+
+		self.save_to_csv(X, Y)
 		
-		X, Y = self.x_y_to_seq(X, Y)
+		# dfX = pd.read_csv('X_preprocessed_data.csv', header=None)
+		# dfY = pd.read_csv('Y_preprocessed_data.csv', header=None)
+		
+		# X, Y = self.x_y_to_seq(dfX.values, dfY.values)
 
-		# what should be the format of Y
-		# regressor? classifier?
+		# # what should be the format of Y
+		# # regressor? classifier?
 
-		return np.array(X), np.array(Y)
+		# return np.array(X), np.array(Y)
 
 		
 	def process_train(self, X, Y):
 
 		# get shape X (N, T, D)
 		X_sample_size, X_seq_size, X_features_size = X.shape
+		print("X_seq_size: ", X_seq_size)
 
 		# get shape Y (K)
 		Y_sample_size = Y.shape
@@ -124,30 +136,45 @@ class MainRNN():
 				X, Y = shuffle(X, Y)				
 				cost = 0
 				accuracy = 0
-				for batch in range(X_sample_size):
-					batchX = X[batch]
-					batchY = Y[batch]
-                    
-					_, cost_out, correct_pred_out = sess.run([optimizer, loss, prediction], feed_dict={tfX: batchX.reshape(X_seq_size, 1, X_features_size), tfY: batchY.reshape(1, self.output_feature_size)})
+				for batch in range(X_sample_size - self.batch_size):
+					batchX = X[batch:batch+self.batch_size]
+					batchY = Y[batch:batch+self.batch_size]
+
+					_, cost_out, prediction_out = sess.run([optimizer, loss, prediction], feed_dict={tfX: batchX.reshape(X_seq_size, self.batch_size, X_features_size), tfY: batchY.reshape(self.batch_size, self.output_feature_size)})
 					
-					correct_pred = tf.cast(tf.less(tf.cast(tf.abs(correct_pred_out - batchY), tf.float64), tf.multiply(batchY, self.error_rate)), tf.float32)
+					correct_pred = tf.cast(tf.less(tf.cast(tf.abs(prediction_out - batchY), tf.float64), tf.multiply(batchY, self.error_rate)), tf.float32)
+					print('cost_out: ',cost_out)
 					cost += cost_out
 					accuracy += correct_pred.eval()[0][0]
-					print('correct_pred_out', correct_pred_out[0][0])
+					print('prediction_out', prediction_out[0][0])
+					print('batchY', batchY)
+					print('correct_pred.eval()[0][0]', correct_pred.eval()[0][0])
 
 				costs.append(cost)
 				accuracies.append(accuracy / (batch + 1))
+				print('cost: ', cost)
 				print('accuracy: ', accuracy)
 				print('batch: ', batch)
 				print('accuracy / batch', accuracy / (batch + 1))
 
-		plt.plot(accuracies)
+		plt.plot(costs)
 		plt.show()
 
 	def run_prediction(self):
 		X, Y = self.get_formated_data()
 		self.process_train(X, Y)
 
+	def save_to_csv(self, X, Y):
+		
+		for row in range(len(X)):
+			np.savetxt("X_preprocessed_data.csv", X, delimiter=",")
+
+
+		for row in range(len(Y)):
+			np.savetxt("Y_preprocessed_data.csv", Y, delimiter=",")
+
 
 if __name__ == '__main__':
-    X, Y = MainRNN().run_prediction()
+	MainRNN().get_formated_data()
+    # X, Y = MainRNN().run_prediction()
+
